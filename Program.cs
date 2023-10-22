6b6a62f34a90201683d6bf8ef035bdb7c32b569b
CyrilJay69 <144930694+CyrilJay69@users.noreply.github.com>
2023-10-22 20:04:57 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.Write("Enter an integer: ");
+            string input = Console.ReadLine();
+            int number = int.Parse(input);
+
+            if (number >= 0)
+            {
+                double squareRoot = Math.Sqrt(number);
+                Console.WriteLine($"Square root of {number} is: {squareRoot}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid number.");
+            }
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid input. Please enter a valid integer.");
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("Input number is too large or too small to be an integer.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+
+        Console.WriteLine("Good Bye.");
+    }
+}
